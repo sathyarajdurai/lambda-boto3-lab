5c9c3ae85ea1af809841a76195815cc77ac9c3b0
sathyarajdurai <sathyaraj.durai@cloudreach.com>
2022-07-29 13:06:50 +0530

    third commit

--- python-scripts/imagereko.py
+++ python-scripts/imagereko.py
@@ -5,8 +5,42 @@ def lambda_handler(event, context):
     client = boto3.client("rekoginition")
 
     for records in event["Records"]:
-        bucket_name = record["s3"]["bucket"]["name"]
-        image_object = record["s3"]["object"]["key"]
+        bucket_name = records["s3"]["bucket"]["name"]
+        image_object = records["s3"]["object"]["key"]
     
-    print(bucket_name)
-    print(image_object)
+
+    amount_of_faces = 0
+    male_face = 0
+    female_face = 0
+
+    print(amount_of_faces)
+    # print(bucket_name)
+    # print(image_obj)
+
+    response = client.detect_faces(Image={'S3Object':{'Bucket':bucket_name,'Name':image_object}},Attributes=['ALL'])
+
+    faces = response["FaceDetails"]
+
+    amount_of_faces = len(faces)
+    print(amount_of_faces)
+
+    for face in faces:
+        if face["Gender"]["Value"] == "Male":
+            male_face += 1
+        elif face["Gender"]["Value"] == "Female":
+            female_face += 1
+
+    table = dynamodb_resource.Table(metadata_table)
+
+    metadata = {
+        "filename": image_obj,
+        "amount_of_faces": amount_of_faces,
+        "male_faces": male_face,
+        "female_faces": female_face
+        # eyesglasses
+        # sunglases
+        # beard
+        # mustache
+    }
+
+    table.put_item(Item=metadata)
--- roles.tf
+++ roles.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "labs-lambda-assume-role"
+  name = "lambda-assume-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -61,3 +61,43 @@ data "aws_iam_policy_document" "lambda_for_dynamodb" {
   }
 }
 
+resource "aws_iam_policy" "lambda_for_rekognition_policy" {
+  name = "lambda-for-rekognition"
+  description = "Allow Lambda function to use rekognition"
+  policy = data.aws_iam_policy_document.lambda_for_rekognition.json
+}   
+
+resource "aws_iam_policy" "lambda_for_s3_access_policy" {
+  name = "lambda-for-s3-access"
+  description = "Allow Lambda function to access S3 bucket and objects"
+  policy = data.aws_iam_policy_document.lambda_for_s3_access.json
+}
+
+resource "aws_iam_policy" "lambda_for_dynamodb_policy" {
+  name = "lambda-for-dynamodb"
+  description = "Allow Lambda function to write item into dynamoDB"
+  policy = data.aws_iam_policy_document.lambda_for_dynamodb.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_rekoginition" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_for_rekognition_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_access_s3" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_for_s3_access_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_write_dynamodb" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_for_dynamodb_policy.arn
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lab_lambda_image_rekognition.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.image_bucket.arn
+}
