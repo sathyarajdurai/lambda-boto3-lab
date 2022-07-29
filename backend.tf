2609b34f8ccc6ae8537a60c183f09abfcba31770
sathyarajdurai <sathyaraj.durai@cloudreach.com>
2022-07-29 12:48:10 +0530

    second commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "talent-academy-sathyaraj-lab-tfstates"
-    key    = "talent-academy/vpc/terraform.tfstates"
+    key    = "talent-academy/lambda/terraform.tfstates"
     region = "eu-west-1"
     dynamodb_table = "terraform-lock"
   }
--- lambda.tf
+++ lambda.tf
@@ -3,3 +3,22 @@ data "archive_file" "python_zip" {
     source_file = "${path.module}/python-scripts/imagereko.py"
     output_path = "${path.module}/lambda-image-function.zip"
 }
+
+resource "aws_lambda_function" "lab_lambda_image_rekognition" {
+  filename      = data.archive_file.python_zip.output_path
+  function_name = imagereko
+
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "imagereko.lambda_handler"
+
+  source_code_hash = filebase64sha256(data.archive_file.python_zip.output_path)
+
+  runtime = "python3.8"
+  timeout = 10
+
+  environment {
+    variables = {
+      "METADATA_TABLE" = aws_dynamodb_table.lambda_image_rekognition.name
+    }
+  }
+}
